779597429670c7c6ef02fb59a9d602fa58810760
zguishen <zguishen@foxmail.com>
2026-09-03 14:39:51 +0800

    Initial Commit

---
+++ CameraImport/CameraImportApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraImportApp.swift
+//  CameraImport
+//
+//  Created by Sylvan on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CameraImportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
